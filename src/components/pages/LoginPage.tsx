b09fda3223174ce9951000288e8bfc2fffbf09ab
ShoheiTakei <mrshohei195@gmail.com>
2021-12-04 14:07:04 +0900

    空文字の時にボタンが押せない様にする処理を実装中

--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 import {
   Box,
   Button,
-  Center,
   ChakraProvider,
   Flex,
   Input,
@@ -16,8 +15,23 @@ import {
 
 export const LoginPage = () => {
   const [showPs, setShowPs] = useState(false);
+  const [saveClick, setSaveClick] = useState(true);
+  const [mail, setMail] = useState('');
+  const [pass, setPass] = useState('');
   const handleClick = () => setShowPs(![showPs]);
-
+  const onChange = (e: { target: { value: React.SetStateAction<string> } }) => {
+    // 条件分岐でメアドとパスワードのinputが中身が空文字ではない場合に保存ボタンが押せる様にする
+    console.log('変更を検知');
+    setMail(e.target.value);
+    console.log(mail);
+    setPass(e.target.value);
+    console.log(pass);
+  };
+  // inputの中身が空文字の場合は、ボタンをクリックできない様にする
+  // bug 表示フラグが発火しない
+  // Enterを押したらボタンが発火する様にする
+  // ボタンを押した時に起こる再レンダリングを防ぐ
+  // メアドとパスが正しい場合は、パスを切り替えて遷移先へ移動する。違う場合はアラートで警告する
   return (
     <>
       <ChakraProvider>
@@ -25,9 +39,15 @@ export const LoginPage = () => {
           <Box align="center" w="400px">
             <Stack spacing={6} mt="300px">
               <Text fontSize="3xl">サインイン</Text>
-              <Input placeholder="メールアドレス" />
+              <Input
+                placeholder="メールアドレス"
+                onChange={onChange}
+                mail={mail}
+              />
               <InputGroup>
                 <Input
+                  onChange={onChange}
+                  pass={pass}
                   type={showPs ? 'text' : 'password'}
                   placeholder="パスワード"
                 />
@@ -37,7 +57,7 @@ export const LoginPage = () => {
                   </Button>
                 </InputRightElement>
               </InputGroup>
-              <Button>保存</Button>
+              <Button isDisabled={saveClick}>保存</Button>
               {/* <Link to="/home" /> */}
             </Stack>
           </Box>
